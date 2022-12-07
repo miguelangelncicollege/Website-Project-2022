761e223d98838afbbbe1491dc4c068948fa8642b
Miguel Angel <x22116133@student.ncirl.ie>
2022-12-07 17:53:24 +0000

    Start of the Website Project for NCI
    Uploading sketch of the website project for NCI

---
+++ javascript/scripts.js
@@ -0,0 +1,11 @@
+function animation()
+{
+	if (document.documentElement.scrollTop > 50)
+		{
+			document.getElementById("idtotalsmsyear").className = "totalsmsyear";
+		}
+	else 
+		{
+			document.getElementById("idtotalsmsyear").className = "";
+		{
+}
