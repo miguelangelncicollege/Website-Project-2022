6baede422f05fb3d429464a93b7cfd2c4f0eb859
Miguel Angel <x22116133@student.ncirl.ie>
2022-12-08 20:43:10 +0000

    Navbar, footer and whole structure mainly done.
    Navbar, footer and whole structure mainly done.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,11 +1,11 @@
-function animation()
+function messageSent()
 {
-	if (document.documentElement.scrollTop > 50)
-		{
-			document.getElementById("idtotalsmsyear").className = "totalsmsyear";
-		}
-	else 
-		{
-			document.getElementById("idtotalsmsyear").className = "";
-		{
+	//alert ("Your message has been sent, we will be in touch soon./nThank you very much.");
+	document.getElementsById("sectioncontactusid").style.display = "none";
+}
+
+function messageNotSent()
+{
+	document.getElementById("messagesent").style.display = "none";
+	document.getElementsById("sectioncontactusid").style.display = "none";
 }
