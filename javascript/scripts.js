0c10eb2acf463b19a76b09b1e910764a6426e00f
Miguel Angel <x22116133@student.ncirl.ie>
2022-12-13 23:54:54 +0000

    Main commit
    Submitting all changes in order to deploy

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,11 +1,4 @@
 function messageSent()
 {
-	//alert ("Your message has been sent, we will be in touch soon./nThank you very much.");
-	document.getElementsById("sectioncontactusid").style.display = "none";
-}
-
-function messageNotSent()
-{
-	document.getElementById("messagesent").style.display = "none";
-	document.getElementsById("sectioncontactusid").style.display = "none";
+	alert ("Your message has been sent, we will be in touch soon.\nThank you very much.");
 }
